c35b669d7fb15fa9e18d755b6be8d8fb080adf4d
sayler <lukasz.chromy@gmail.com>
2017-02-17 03:33:55 +0100

    switch to OrmLite module

--- app/src/main/java/com/sayler/gina/dagger/component/DataComponent.java
+++ app/src/main/java/com/sayler/gina/dagger/component/DataComponent.java
@@ -4,7 +4,7 @@ import com.sayler.gina.activity.DayActivity;
 import com.sayler.gina.activity.DayEditActivity;
 import com.sayler.gina.activity.MainActivity;
 import com.sayler.gina.dagger.module.ServicesModule;
-import com.sayler.gina.dagger.module.realm.DataModuleRealm;
+import com.sayler.gina.dagger.module.ormlite.DataModuleOrmLite;
 import dagger.Component;
 
 import javax.inject.Singleton;
@@ -12,7 +12,7 @@ import javax.inject.Singleton;
 @Singleton
 @Component(
     modules = {
-        DataModuleRealm.class,
+        DataModuleOrmLite.class,
         ServicesModule.class
     },
     dependencies = {
