de8c8f42cd2ace529236115070343d6507756ef9
sayler666 <lukasz.chromy@gmail.com>
2017-05-23 13:39:20 +0200

    SharedPreferenceHelper to kotlin

--- app/src/main/java/com/sayler/gina/store/settings/SPSettingsStoreManager.kt
+++ app/src/main/java/com/sayler/gina/store/settings/SPSettingsStoreManager.kt
@@ -1,25 +1,25 @@
 package com.sayler.gina.store.settings
 
 import android.content.Context
-import com.sayler.gina.store.SharedPreferencesHelper
+import com.sayler.gina.store.SharedPreferenceHelper
 import com.sayler.gina.util.Constants
 
 import javax.inject.Singleton
 
 @Singleton
 class SPSettingsStoreManager(context: Context) : SettingsStoreManager {
-    var sharedPreferencesHelper: SharedPreferencesHelper<SettingsStore> = SharedPreferencesHelper(context, Constants.PREF_SETTINGS_STORE, SettingsStore::class.java)
+    var sharedPreferenceHelper: SharedPreferenceHelper<SettingsStore> = SharedPreferenceHelper(context, Constants.PREF_SETTINGS_STORE, SettingsStore::class.java)
 
     override fun get(): SettingsStore? {
-        return sharedPreferencesHelper.retrieve()
+        return sharedPreferenceHelper.retrieve()
     }
 
     override fun save(`object`: SettingsStore) {
-        sharedPreferencesHelper.store(`object`)
+        sharedPreferenceHelper.store(`object`)
     }
 
     override fun clear() {
-        sharedPreferencesHelper.clear()
+        sharedPreferenceHelper.clear()
     }
 
 }
