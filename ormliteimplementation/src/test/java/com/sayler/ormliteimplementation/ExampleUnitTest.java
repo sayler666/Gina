9a581eea913b547bc79d2f9b1064cf95b9ed5373
sayler666 <lukasz.chromy@gmail.com>
2017-11-30 12:29:49 +0100

    TravisCI test failed

--- ormliteimplementation/src/test/java/com/sayler/ormliteimplementation/ExampleUnitTest.java
+++ ormliteimplementation/src/test/java/com/sayler/ormliteimplementation/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.sayler.ormliteimplementation;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,10 @@ public class ExampleUnitTest {
   @Test
   public void addition_isCorrect() throws Exception {
     assertEquals(4, 2 + 2);
+  }
+
+  @Test
+  public void addition_isCorrect_failed() throws Exception {
+    assertEquals(4, 2 + 1);
   }
 }
