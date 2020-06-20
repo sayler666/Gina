b0347c30008c8ed311d9f2e38e068d8662de1fc1
sayler666 <lukasz.chromy@gmail.com>
2020-06-20 16:13:04 +0200

    From scratch

--- domain/src/main/java/com/sayler/gina/domain/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.sayler.gina.domain
-
-object Constants {
-
-    val PREFERENCES = "PREFERENCES_KEY"
-    val PREF_SETTINGS_STORE = "PREF_SETTINGS_STORE"
-}
--- domain/src/main/java/com/sayler/gina/domain/stats/StatisticPair.kt
+++
@@ -1,6 +0,0 @@
-package com.sayler.gina.domain.stats
-
-/**
- * Created by sayler on 03.10.2017.
- */
-data class StatisticPair(val label: String, val value: String)
