2f6002d65a2d396646edf3da2450932afd5f4f49
sayler666 <lukasz.chromy@gmail.com>
2023-03-21 19:53:44 +0100

    Settings: Friends management: full screen instead of dialog

--- app/src/main/java/com/sayler666/gina/settings/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/sayler666/gina/settings/viewmodel/SettingsViewModel.kt
@@ -2,73 +2,23 @@ package com.sayler666.gina.settings.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sayler666.gina.dayDetails.viewmodel.FriendEntity
 import com.sayler666.gina.db.DatabaseProvider
-import com.sayler666.gina.friends.usecase.AddFriendUseCase
-import com.sayler666.gina.friends.usecase.GetAllFriendsUseCase
-import com.sayler666.gina.friends.viewmodel.FriendsMapper
 import com.sayler666.gina.imageCompressor.ImageCompressor.CompressorSettings
 import com.sayler666.gina.settings.Settings
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val setting: Settings,
-    getAllFriendsUseCase: GetAllFriendsUseCase,
-    friendsMapper: FriendsMapper,
-    private val addFriendUseCase: AddFriendUseCase,
     private val databaseProvider: DatabaseProvider
 ) : ViewModel() {
-
-    private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
-        Timber.e(exception)
-    }
-
-    private val _friends = getAllFriendsUseCase.getAllFriends().stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(500),
-        emptyList()
-    )
-
-    private val friendsSearchQuery: MutableStateFlow<String?> = MutableStateFlow(null)
-    val friends: StateFlow<List<FriendEntity>> = combine(
-        _friends,
-        friendsSearchQuery
-    ) { friends, friendsSearchQuery ->
-        friendsMapper.mapToFriends(friends, friendsSearchQuery)
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(500),
-        emptyList()
-    )
-
-    fun searchFriend(searchQuery: String) {
-        friendsSearchQuery.update { searchQuery }
-    }
-
-    fun addNewFriend(friendName: String) {
-        viewModelScope.launch(SupervisorJob() + exceptionHandler) {
-            addFriendUseCase.addFriend(friendName)
-        }
-    }
-
-    fun removeFriend(friendId: Int) {
-        viewModelScope.launch(SupervisorJob() + exceptionHandler) {
-            //addFriendUseCase.remo(friendName)
-        }
-    }
 
     private val _tempImageCompressorSettings: MutableStateFlow<CompressorSettings> =
         MutableStateFlow(CompressorSettings())
