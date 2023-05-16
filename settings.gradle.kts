7c316177f91354f472d4d83740a347503f19f795
sayler666 <lukasz.chromy@gmail.com>
2023-05-16 19:50:37 +0200

    Migrate to Gradle version catalogs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,19 @@
 rootProject.name = "Gina"
 include(":app")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
