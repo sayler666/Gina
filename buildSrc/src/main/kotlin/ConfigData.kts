18d1462e0c414f4d32d6b9feab118a79e70094ea
sayler666 <lukasz.chromy@gmail.com>
2023-09-06 11:51:33 +0200

    version update: 2.4.0

--- buildSrc/src/main/kotlin/ConfigData.kts
+++ buildSrc/src/main/kotlin/ConfigData.kts
@@ -1,7 +1,7 @@
 object ConfigData {
     const val compileSdkVersion = "android-34"
     const val minSdk = 31
-    const val versionCode = 4
-    const val versionName = "2.3.0"
+    const val versionCode = 5
+    const val versionName = "2.4.0"
     const val applicationId = "com.sayler666.gina"
 }
