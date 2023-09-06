4c65800a43521f247517e7a7f62dd400d830528f
sayler666 <lukasz.chromy@gmail.com>
2023-09-06 15:44:21 +0200

    Fix FullImageDialog scale issues

--- app/src/main/java/com/sayler666/gina/ui/ZoomableBox.kt
+++ app/src/main/java/com/sayler666/gina/ui/ZoomableBox.kt
@@ -1,11 +1,13 @@
 package com.sayler666.gina.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -14,21 +16,28 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.IntSize
+import timber.log.Timber
 
 @Composable
 fun ZoomableBox(
     modifier: Modifier = Modifier,
     minScale: Float = 1f,
     maxScale: Float = 7f,
-    outsideImageClick: (() -> Unit)? = null,
+    click: (() -> Unit)? = null,
     originalImageHeight: Int,
     originalImageWidth: Int,
     content: @Composable ZoomableBoxScope.() -> Unit
 ) {
-    var scale by remember { mutableStateOf(1f) }
-    var offsetX by remember { mutableStateOf(0f) }
-    var offsetY by remember { mutableStateOf(0f) }
+    val configuration = LocalConfiguration.current
+    val orientationPortrait by remember {
+        mutableStateOf(configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+    }
+
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
     var size by remember { mutableStateOf(IntSize.Zero) }
     val interactionSource = remember { MutableInteractionSource() }
 
@@ -37,20 +46,35 @@ fun ZoomableBox(
             .clip(RectangleShape)
             .onSizeChanged {
                 size = it
-                val asp = it.width.toFloat() / originalImageWidth
-                offsetY += (it.height - originalImageHeight.toFloat() * asp) / 2
+                if (orientationPortrait) {
+                    val asp = size.width.toFloat() / originalImageWidth
+                    offsetY += (size.height - originalImageHeight.toFloat() * asp) / 2
+                } else {
+                    val asp = size.height.toFloat() / originalImageHeight
+                    offsetX += (size.width - originalImageWidth.toFloat() * asp) / 2
+                }
+                Timber.d("Image: offsetY: $offsetY")
+                Timber.d("Image: offsetX: $offsetX")
             }
             .pointerInput(Unit) {
                 detectTransformGestures { _, pan, zoom, _ ->
-                    scale = maxOf(minScale, minOf(scale * zoom, maxScale))
-                    val maxX = (size.width * (scale - 1)) / 2
-                    val minX = -maxX
-                    offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
-                    offsetY += pan.y
+                    if (orientationPortrait) {
+                        scale = maxOf(minScale, minOf(scale * zoom, maxScale))
+                        val maxX = (size.width * (scale - 1)) / 2
+                        val minX = -maxX
+                        offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
+                        offsetY += pan.y
+                    } else {
+                        scale = maxOf(minScale, minOf(scale * zoom, maxScale))
+                        val maxY = (size.width * (scale - 1)) / 2
+                        val minY = -maxY
+                        offsetY = maxOf(minY, minOf(maxY, offsetY + pan.y))
+                        offsetX += pan.x
+                    }
                 }
             }
             .clickable(interactionSource = interactionSource, indication = null) {
-                outsideImageClick?.invoke()
+                click?.invoke()
             }
     ) {
         val scope = ZoomableBoxScopeImpl(scale, offsetX, offsetY)
