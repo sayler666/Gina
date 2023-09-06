4e790f8782170acdf15120584002f8f099904774
sayler666 <lukasz.chromy@gmail.com>
2023-09-06 15:44:21 +0200

    Fix FullImageDialog scale issues

--- app/src/main/java/com/sayler666/gina/attachments/ui/FullImageDialog.kt
+++ app/src/main/java/com/sayler666/gina/attachments/ui/FullImageDialog.kt
@@ -1,25 +1,36 @@
 package com.sayler666.gina.attachments.ui
 
-import android.content.res.Configuration
-import android.graphics.BitmapFactory
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.OpenInNew
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -28,12 +39,14 @@ import coil.compose.rememberAsyncImagePainter
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.sayler666.core.file.Files
+import com.sayler666.core.image.scaleToMinSize
 import com.sayler666.gina.dayDetails.ui.DayDetailsScreenNavArgs
 import com.sayler666.gina.destinations.DayDetailsScreenDestination
-import com.sayler666.gina.ui.FullScreenDialog
+import com.sayler666.gina.ui.NavigationBarColor
+import com.sayler666.gina.ui.StatusBarColor
 import com.sayler666.gina.ui.ZoomableBox
 
-@Destination(style = FullScreenDialog::class)
+@Destination
 @Composable
 fun FullImageDialog(
     destinationsNavigator: DestinationsNavigator,
@@ -41,31 +54,35 @@ fun FullImageDialog(
     mimeType: String,
     dayId: Int? = null
 ) {
-    val configuration = LocalConfiguration.current
     val context = LocalContext.current
-    val (bitmapWidth, bitmapHeight) = BitmapFactory.decodeByteArray(image, 0, image.size).let {
-        val (w, h) = it.width to it.height
-        it.recycle()
-        w to h
-    }
+    val scaledBitmapInfo = image.scaleToMinSize()
+
+    StatusBarColor(color = Color.Transparent, theme = null)
+    NavigationBarColor(color = Color.Transparent, theme = null)
+
+    var bottomBarVisible by remember { mutableStateOf(true) }
+
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
             .padding(0.dp)
     ) {
         val (zoomableBox, bottomBar) = createRefs()
+
         ZoomableBox(
             modifier = Modifier
                 .fillMaxWidth()
                 .constrainAs(zoomableBox) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
-                    bottom.linkTo(bottomBar.top)
+                    bottom.linkTo(parent.bottom)
                     height = Dimension.fillToConstraints
                 },
-            originalImageHeight = bitmapHeight,
-            originalImageWidth = bitmapWidth,
-            outsideImageClick = { destinationsNavigator.popBackStack() }) {
+            originalImageHeight = scaledBitmapInfo.height,
+            originalImageWidth = scaledBitmapInfo.width,
+            click = {
+                bottomBarVisible = !bottomBarVisible
+            }) {
             Image(
                 modifier = Modifier
                     .graphicsLayer(
@@ -74,19 +91,40 @@ fun FullImageDialog(
                         translationX = offsetX,
                         translationY = offsetY
                     ),
-                painter = rememberAsyncImagePainter(image),
+                painter = rememberAsyncImagePainter(scaledBitmapInfo.bitmap),
                 contentDescription = null
             )
         }
-        if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) BottomAppBar(
+        AnimatedVisibility(
+            visible = bottomBarVisible,
+            enter = slideInVertically(
+                initialOffsetY = { it },
+                animationSpec = tween(durationMillis = 80, easing = FastOutLinearInEasing)
+            ),
+            exit = slideOutVertically(
+                targetOffsetY = { it },
+                animationSpec = tween(durationMillis = 80, easing = FastOutLinearInEasing)
+            ),
             modifier = Modifier
                 .fillMaxWidth()
+                .navigationBarsPadding()
                 .constrainAs(bottomBar) {
                     bottom.linkTo(parent.bottom)
                     start.linkTo(parent.start)
-                },
-            containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp),
-            actions = {
+                }
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .navigationBarsPadding()
+                    .height(60.dp)
+                    .background(
+                        MaterialTheme.colorScheme
+                            .surfaceColorAtElevation(3.dp)
+                            .copy(alpha = 0.2f)
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 IconButton(onClick = {
                     destinationsNavigator.popBackStack()
                     Files.openFileIntent(context, bytes = image, mimeType = mimeType)
@@ -107,10 +145,15 @@ fun FullImageDialog(
                 }
 
                 Spacer(modifier = Modifier.weight(1f))
+
+                val mimeTypeRegexp = Regex(".+/(\\w+)")
+                val type = mimeType.replace(mimeTypeRegexp) { it.groupValues[1] }
+
                 Text(
-                    text = "$mimeType : ${image.size / 1024}KB",
+                    text = "$type: ${image.size / 1024}KB",
                     modifier = Modifier.padding(end = 8.dp)
                 )
-            })
+            }
+        }
     }
 }
---
+++ core/src/main/java/com/sayler666/core/image/ImageUtil.kt
@@ -0,0 +1,73 @@
+package com.sayler666.core.image
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import timber.log.Timber
+
+data class ScaledBitmapInfo(
+    val bitmap: Bitmap,
+    val width: Int,
+    val height: Int
+)
+
+fun ByteArray.scaleToMinSize(minWidth: Int = 1080, minHeight: Int = 1920): ScaledBitmapInfo {
+    var scaledBitmap: Bitmap?
+
+    BitmapFactory.decodeByteArray(
+        this,
+        0,
+        this.size
+    ).let {
+        var (w, h) = it.width to it.height
+
+        Timber.d("Image: Source size: w: $w, h: $h")
+        if (it.width < it.height && it.width < 1080) {
+            val m = android.graphics.Matrix()
+            val ratio = it.height / it.width.toFloat()
+            val dstHeight = minWidth * ratio
+            Timber.d("Image: Ratio: $ratio")
+            Timber.d("Image: dst: w: $minWidth, h: $dstHeight")
+            if (w != minWidth || h.toFloat() != dstHeight) {
+                val sx = minWidth / w.toFloat()
+                val sy = dstHeight / h.toFloat()
+                m.setScale(sx, sy)
+            }
+            scaledBitmap = Bitmap.createBitmap(it, 0, 0, w, h, m, true)
+            w = minWidth
+            h = dstHeight.toInt()
+        } else if (it.height < it.width && it.height < 1920) {
+            val m = android.graphics.Matrix()
+            val ratio = it.width.toFloat() / it.height
+            val destWidth = minHeight * ratio
+            Timber.d("Image: Ratio: $ratio")
+            Timber.d("Image: dst: w: $destWidth, h: $minHeight")
+            if (w.toFloat() != destWidth || h != minHeight) {
+                val sx = destWidth / w.toFloat()
+                val sy = minHeight / h.toFloat()
+                m.setScale(sx, sy)
+            }
+            scaledBitmap = Bitmap.createBitmap(it, 0, 0, w, h, m, true)
+            w = destWidth.toInt()
+            h = minHeight
+        } else if (it.height == it.width && it.height < 1080) {
+            val m = android.graphics.Matrix()
+            val ratio = it.width.toFloat() / it.height
+            val destWidth = minHeight * ratio
+            Timber.d("Image: Ratio: $ratio")
+            Timber.d("Image: dst: w: $destWidth, h: $minHeight")
+            if (w.toFloat() != destWidth || h != minHeight) {
+                val sx = destWidth / w.toFloat()
+                val sy = minHeight / h.toFloat()
+                m.setScale(sx, sy)
+            }
+            scaledBitmap = Bitmap.createBitmap(it, 0, 0, w, h, m, true)
+            w = destWidth.toInt()
+            h = minHeight
+        } else {
+            scaledBitmap = it.copy(it.config, true)
+        }
+
+        it.recycle()
+        return ScaledBitmapInfo(scaledBitmap!!, w, h)
+    }
+}
