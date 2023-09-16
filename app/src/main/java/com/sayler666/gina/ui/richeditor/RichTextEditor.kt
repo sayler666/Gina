adcb9c03f1a33af3b9f70c615f01cbf5aef1602f
sayler666 <lukasz.chromy@gmail.com>
2023-09-16 12:57:37 +0200

    Append quote to day content

--- app/src/main/java/com/sayler666/gina/ui/richeditor/RichTextEditor.kt
+++ app/src/main/java/com/sayler666/gina/ui/richeditor/RichTextEditor.kt
@@ -1,10 +1,16 @@
 package com.sayler666.gina.ui.richeditor
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,10 +28,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.LinearGradientShader
+import androidx.compose.ui.graphics.Shader
+import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.mohamedrejeb.richeditor.model.RichTextState
@@ -43,9 +58,6 @@ fun RichTextEditor(
     quote: Quote? = null,
     onContentChanged: (String) -> Unit
 ) {
-    var blurEnabled by remember { mutableStateOf(true) }
-    val blurRadius: Dp by animateDpAsState(if (blurEnabled) 30.dp else 0.dp, tween(500))
-
     val focusRequester = remember { FocusRequester() }
     if (autoFocus) LaunchedEffect(Unit) { focusRequester.requestFocus() }
 
@@ -64,41 +76,120 @@ fun RichTextEditor(
         textStyle = MaterialTheme.typography.bodyLarge.copy(color = colorScheme.onSurface),
         cursorBrush = SolidColor(colorScheme.primary),
         decorationBox = { innerTextField ->
-            AnimatedVisibility(
+            QuoteWithAuthor(
+                quote = quote,
                 visible = richTextState.toHtml().getTextWithoutHtml()
                     .isEmpty() && quote != null,
-                enter = fadeIn(),
-                exit = fadeOut()
-            ) {
-                if (quote != null) {
-                    blurEnabled = false
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .blur(blurRadius)
-                    ) {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = quote.quote,
-                            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                            fontWeight = FontWeight.Normal,
-                            color = colorScheme.onSurface.copy(alpha = 0.6f),
-                            textAlign = TextAlign.Center
-                        )
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "—${quote.author}",
-                            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-                            fontWeight = FontWeight.Normal,
-                            color = colorScheme.onSurface.copy(alpha = 0.8f),
-                            textAlign = TextAlign.Center
-                        )
+                onClick = { quote ->
+                    with(richTextState) {
+                        setHtml(quote.toHtml())
+                        removeSpanStyle(currentSpanStyle)
+                        removeParagraphStyle(currentParagraphStyle)
                     }
                 }
-            }
+            )
             innerTextField()
         }
     )
+}
+
+private fun Quote.toHtml() = "<p style=\"text-align: center;\"><i>$quote<i></p>" +
+        "<p style=\"text-align: center;\"><b>—$author</b></p>" +
+        "<p><br/></p>"
+
+@Preview(backgroundColor = 0xFF009688)
+@Composable
+fun QuotePreview() {
+    QuoteWithAuthor(
+        quote = Quote(
+            id = null,
+            quote = "Lorem ipsum et cetera, lorem et ipum, lorem et ipum.",
+            author = "sayler",
+            date = 100L
+        ),
+        visible = true,
+        onClick = {}
+    )
+}
+
+@Composable
+private fun QuoteWithAuthor(
+    quote: Quote?,
+    visible: Boolean,
+    onClick: (Quote) -> Unit
+) {
+    var blurEnabled by remember { mutableStateOf(true) }
+    val blurRadius: Dp by animateDpAsState(if (blurEnabled) 30.dp else 0.dp, tween(500))
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val offsetTarget = 3000f
+    val offset by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = offsetTarget,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 4000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = "gradient offset"
+    )
+    val gradient = listOf(
+        colorScheme.onSurface.copy(alpha = 0.6f),
+        Color.White.copy(alpha = 0.8f),
+        colorScheme.onSurface.copy(alpha = 0.6f)
+        )
+    val brush = remember(offset) {
+        object : ShaderBrush() {
+            override fun createShader(size: Size): Shader {
+                val times = offsetTarget / size.width
+                return LinearGradientShader(
+                    colors = gradient,
+                    from = Offset(offset - size.width, offset),
+                    to = Offset(
+                        size.width * times + offset - size.width,
+                        size.width * times + offset
+                    ),
+                    tileMode = TileMode.Repeated,
+                    colorStops = listOf(0.05f, 0.1f, 0.15f)
+                )
+            }
+        }
+    }
+
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        if (quote != null) {
+            blurEnabled = false
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .blur(blurRadius)
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onClick(quote) },
+                    text = quote.quote,
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        brush = brush
+                    )
+                )
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { onClick(quote) },
+                    text = "—${quote.author}",
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                    fontWeight = FontWeight.Normal,
+                    color = colorScheme.onSurface.copy(alpha = 0.8f),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
 
 fun RichTextState.setTextOrHtml(text: String) =
