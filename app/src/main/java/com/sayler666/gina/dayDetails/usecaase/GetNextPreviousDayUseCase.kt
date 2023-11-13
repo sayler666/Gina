a123863127b8bf7bc9047fdbe9e68801d0ea8de1
Łukasz Chromy <lukasz.chromy@gmail.com>
2023-11-13 20:11:51 +0100

    Fix navigation to next/previous day in case of day duplication

--- app/src/main/java/com/sayler666/gina/dayDetails/usecaase/GetNextPreviousDayUseCase.kt
+++ app/src/main/java/com/sayler666/gina/dayDetails/usecaase/GetNextPreviousDayUseCase.kt
@@ -1,23 +1,24 @@
 package com.sayler666.gina.dayDetails.usecaase
 
 import android.database.SQLException
+import com.sayler666.gina.db.Day
 import com.sayler666.gina.db.GinaDatabaseProvider
 import com.sayler666.gina.db.returnWithDaysDao
 import timber.log.Timber
-import java.time.LocalDate
 import javax.inject.Inject
 
 interface GetNextPreviousDayUseCase {
-    suspend fun getNextDayAfterDate(localDate: LocalDate): Result<Int>
-    suspend fun getPreviousDayBeforeDate(localDate: LocalDate): Result<Int>
+    suspend fun getNextDayAfterDate(day: Day): Result<Int>
+    suspend fun getPreviousDayBeforeDate(day: Day): Result<Int>
 }
 
 class GetNextPreviousDayUseCaseUseCaseImpl @Inject constructor(
     private val ginaDatabaseProvider: GinaDatabaseProvider
 ) : GetNextPreviousDayUseCase {
-    override suspend fun getNextDayAfterDate(localDate: LocalDate): Result<Int> = try {
+    override suspend fun getNextDayAfterDate(day: Day): Result<Int> = try {
         val dayId: Int? = ginaDatabaseProvider.returnWithDaysDao {
-            getNextDayIdAfter(localDate)
+            if (day.id != null && day.date != null)
+                getNextDayIdAfter(day.date, day.id) else null
         }
         dayId?.let { Result.success(it) }
             ?: Result.failure(NoSuchElementException("No next day found"))
@@ -26,9 +27,10 @@ class GetNextPreviousDayUseCaseUseCaseImpl @Inject constructor(
         Result.failure(e)
     }
 
-    override suspend fun getPreviousDayBeforeDate(localDate: LocalDate): Result<Int> = try {
+    override suspend fun getPreviousDayBeforeDate(day: Day): Result<Int> = try {
         val dayId: Int? = ginaDatabaseProvider.returnWithDaysDao {
-            getPreviousDayIdBefore(localDate)
+            if (day.id != null && day.date != null)
+                getPreviousDayIdBefore(day.date, day.id) else null
         }
         dayId?.let { Result.success(it) }
             ?: Result.failure(NoSuchElementException("No previous day found"))
