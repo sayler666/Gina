8fe95bf34df9257556777650d460848466c8f141
Łukasz Chromy <lukasz.chromy@gmail.com>
2024-03-13 19:58:26 +0100

    Update version to 2.7.0

--- buildSrc/src/main/kotlin/ConfigData.kts
+++ buildSrc/src/main/kotlin/ConfigData.kts
@@ -2,6 +2,6 @@ object ConfigData {
     const val compileSdkVersion = "android-34"
     const val minSdk = 33
     const val versionCode = 8
-    const val versionName = "2.6.0"
+    const val versionName = "2.7.0"
     const val applicationId = "com.sayler666.gina"
 }
